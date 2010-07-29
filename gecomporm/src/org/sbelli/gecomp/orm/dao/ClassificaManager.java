f67ddbdde97b43aeb4b4585f0b369ad2b950d6ec
71862 <71862@localhost>
2010-07-29 10:44:49 +0200

    [71862] - FS#76 - BE : Implementare struttura a servizi

--- gecomporm/src/org/sbelli/gecomp/orm/dao/ClassificaManager.java
+++ gecomporm/src/org/sbelli/gecomp/orm/dao/ClassificaManager.java
@@ -1,6 +1,9 @@
 package org.sbelli.gecomp.orm.dao;
 
-
+/**
+ * @author sbelli
+ * @deprecated tutte queste attivita' devono essere fatte a front-end
+ */
 public class ClassificaManager {
 	
 //	public ClassificaGara getClassificaGeneraleGara(Gara gara, List<Categoria> categorie, List<Prestazione> classificaGeneraleGara) {
