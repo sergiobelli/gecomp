7ed33f5615c0311215c3e5e04690e714642ee66e
71862 <71862@localhost>
2010-07-29 10:45:12 +0200

    [71862] - FS#76 - BE : Implementare struttura a servizi

---
+++ gecompshared/src/net/sb/gecomp/exceptions/GeCompOrmException.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package net.sb.gecomp.exceptions;
+
+
+/**
+ * @author S.BELLI
+ *
+ */
+public class GeCompOrmException extends GeCompSrvException {
+
+	public GeCompOrmException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public GeCompOrmException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GeCompOrmException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GeCompOrmException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
