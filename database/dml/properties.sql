4c3d992498dc64e0f617ef87353325e087002ee7
sbelli <sbelli@localhost>
2010-08-09 23:39:42 +0200

    mer

--- database/dml/properties.sql
+++ database/dml/properties.sql
@@ -4,7 +4,7 @@
 
 INSERT INTO `properties` (`ID`, `CHIAVE`, `VALORE`) VALUES
 (1, 'gecomp.session.offset', '30'),
-(2, 'gecomp.staging.area', '|Programmi|java|servers|apache-tomcat-6.0.26|logs|GeComp|classifiche'),
+(2, 'gecomp.staging.area', 'Programmi|java|servers|apache-tomcat-6.0.26|logs|GeComp|classifiche'),
 (3, 'gecomp.start.path', '|home|sbelli'),
 (4, 'gecomp.report.file.extension', '.xls'),
 (5, 'CHIAVE_4', 'VALORE_4');
