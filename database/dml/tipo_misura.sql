3e98f8d91315a06c5466715781619849c99c384b
sbelli <sbelli@localhost>
2010-08-09 22:42:06 +0200

    [sbelli] - Export puntuale tabella TIPO_MISURA

---
+++ database/dml/tipo_misura.sql
@@ -0,0 +1,9 @@
+--
+-- Dump dei dati per la tabella `tipo_misura`
+--
+
+INSERT INTO `tipo_misura` (`ID_TIPO_MISURA`, `DESCRIZIONE`, `UNITA_MISURA`, `MODALITA_COMPARAZIONE`) VALUES
+(1, 'Tempo', 'millisecondi', 0),
+(2, 'Lancio', 'metri', 1),
+(3, 'Salto', 'metri', 1),
+(4, 'Ordine di arrivo', 'posizione', 0);
