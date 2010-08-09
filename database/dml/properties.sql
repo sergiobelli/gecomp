816b3e7211d28ab0361249044c455880c5b3a483
sbelli <sbelli@localhost>
2010-08-09 22:41:03 +0200

    [sbelli] - FS#97 - GUI Classifica Individuale : Errore nella creazione delle cartelle di salvataggio report

---
+++ database/dml/properties.sql
@@ -0,0 +1,10 @@
+--
+-- Dump dei dati per la tabella `properties`
+--
+
+INSERT INTO `properties` (`ID`, `CHIAVE`, `VALORE`) VALUES
+(1, 'gecomp.session.offset', '30'),
+(2, 'gecomp.staging.area', '|Programmi|java|servers|apache-tomcat-6.0.26|logs|GeComp|classifiche'),
+(3, 'gecomp.start.path', '|home|sbelli'),
+(4, 'gecomp.report.file.extension', '.xls'),
+(5, 'CHIAVE_4', 'VALORE_4');
