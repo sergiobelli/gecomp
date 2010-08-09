69ca96012afd9794d6a4c4d3e37130e477040511
sbelli <sbelli@localhost>
2010-08-09 22:42:20 +0200

    [sbelli] - Export puntuale tabella TIPO_PRESTAZIONE

---
+++ database/dml/tipo_prestazione.sql
@@ -0,0 +1,11 @@
+--
+-- Dump dei dati per la tabella `tipo_prestazione`
+--
+
+INSERT INTO `tipo_prestazione` (`ID_TIPO_PRESTAZIONE`, `DESCRIZIONE`) VALUES
+(1, 'CLASSIFICATO'),
+(2, 'RITIRATO'),
+(3, 'SQUALIFICATO'),
+(4, 'NON CLASSIFICATO'),
+(5, 'NON PARTECIPATO'),
+(6, 'NON COMPETITIVO');
