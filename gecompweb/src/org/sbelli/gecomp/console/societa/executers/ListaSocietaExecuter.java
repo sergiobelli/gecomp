83916b8edd024b775933875e2049b3899d0ca81f
71862 <71862@localhost>
2010-08-11 15:22:40 +0200

    [71862] - FS#99 - GUI in generale : Gestione fac simile a GUI Iscrizioni

--- gecompweb/src/org/sbelli/gecomp/console/societa/executers/ListaSocietaExecuter.java
+++ gecompweb/src/org/sbelli/gecomp/console/societa/executers/ListaSocietaExecuter.java
@@ -2,7 +2,6 @@ package org.sbelli.gecomp.console.societa.executers;
 
 import net.sb.gecomp.exceptions.GeCompException;
 import net.sb.gecomp.utils.exceptions.GeCompExceptionManager;
-import net.sb.gecomp.utils.logger.GeCompLogger;
 
 public class ListaSocietaExecuter extends SocietaExecuter {
 		
@@ -10,7 +9,7 @@ public class ListaSocietaExecuter extends SocietaExecuter {
 		try {
 			delegate.save(getSocieta());
 		} catch (GeCompException e) {
-			GeCompExceptionManager.manageException(logger, e);
+			GeCompExceptionManager.traceException(logger, e);
 		}	
 		return "";
 	}
