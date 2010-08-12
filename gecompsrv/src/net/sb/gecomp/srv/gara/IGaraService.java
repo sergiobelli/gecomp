dc054f764bab59e9170626340e123e8ffe10105f
71862 <71862@localhost>
2010-08-12 16:37:37 +0200

    [71862] - FS#41 - Rinominare packages FS#62 - Bridge - creare classi View di binding con strato Orm FS#76 - BE : Implementare struttura a servizi FS#98 - PrestazionView FS#99 - GUI in generale : Gestione fac simile a GUI Iscrizioni

--- gecompsrv/src/net/sb/gecomp/srv/gara/IGaraService.java
+++
@@ -1,9 +0,0 @@
-package net.sb.gecomp.srv.gara;
-
-import net.sb.gecomp.exceptions.GeCompSrvException;
-import net.sb.gecomp.model.Gara;
-
-
-public interface IGaraService {
-	Gara save(Gara gara) throws GeCompSrvException;
-}
