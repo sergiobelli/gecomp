3b4b201a360b34072ae20035adde2f4644ade900
71862 <71862@localhost>
2010-08-27 16:56:41 +0200

    [71862] - FS#76 - BE : Implementare struttura a servizi

--- gecomptest/src/net/sb/gecomp/web/iscrizioni/controllers/IscrizioneControllerTest.java
+++ gecomptest/src/net/sb/gecomp/web/iscrizioni/controllers/IscrizioneControllerTest.java
@@ -2,15 +2,29 @@ package net.sb.gecomp.web.iscrizioni.controllers;
 
 import static org.junit.Assert.fail;
 
+import net.sb.gecomp.commons.exceptions.GeCompException;
+import net.sb.gecomp.commons.model.view.IscrizioneView;
+import net.sb.gecomp.commons.utils.Eval;
+
 import org.junit.Test;
 
 public class IscrizioneControllerTest {
 
 	@Test
-	public void testCheckAtleta() {
+	public void testCheckAtleta_ok() {
 		fail("Not yet implemented");
+		protected void checkAtleta(IscrizioneView iscrizione) throws GeCompException {
+			if (Eval.isNull(iscrizione.getAtleta())
+					|| Eval.isNull(iscrizione.getAtleta().getIdAtleta())) {
+				throw new GeCompException("net.sb.gecomp.console.iscrizioni.controllers.error.atleta.empty");
+			}
+		}
 	}
-
+	
+	public void testCheckAtleta_ok() {
+		
+	}
+	
 	@Test
 	public void testCheckGara() {
 		fail("Not yet implemented");
