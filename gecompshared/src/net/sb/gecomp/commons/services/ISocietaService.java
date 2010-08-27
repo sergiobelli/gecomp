9f42e29022a03a077de092744d91c03d0d512d19
71862 <71862@localhost>
2010-08-27 16:56:58 +0200

    [71862] - FS#76 - BE : Implementare struttura a servizi

--- gecompshared/src/net/sb/gecomp/commons/services/ISocietaService.java
+++ gecompshared/src/net/sb/gecomp/commons/services/ISocietaService.java
@@ -1,7 +1,24 @@
 package net.sb.gecomp.commons.services;
 
+import java.util.List;
+
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+import net.sb.gecomp.commons.exceptions.GeCompSrvException;
 import net.sb.gecomp.commons.model.Societa;
 
-public interface ISocietaService extends IService<Societa> {
+@WebService
+public interface ISocietaService /*extends IService<Societa>*/ {
 
+	Societa save(@WebParam(name="societa") Societa societa) throws GeCompSrvException;
+
+	Societa get(@WebParam(name="id") Long id) throws GeCompSrvException;
+	
+	void delete(@WebParam(name="id") Long id) throws GeCompSrvException;
+	
+	List<Societa> list() throws GeCompSrvException; 
+	
 }
+
+
