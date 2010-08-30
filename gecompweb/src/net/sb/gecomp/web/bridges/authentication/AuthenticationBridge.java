531dfbc0654ac9a71590c511706f3d0e3fb6719f
71862 <71862@localhost>
2010-08-30 17:04:24 +0200

    [71862] - FS#76 - BE : Implementare struttura a servizi

--- gecompweb/src/net/sb/gecomp/web/bridges/authentication/AuthenticationBridge.java
+++ gecompweb/src/net/sb/gecomp/web/bridges/authentication/AuthenticationBridge.java
@@ -3,15 +3,17 @@ package net.sb.gecomp.web.bridges.authentication;
 import net.sb.gecomp.commons.exceptions.GeCompException;
 import net.sb.gecomp.commons.model.view.UserView;
 import net.sb.gecomp.commons.services.IAuthenticationService;
-import net.sb.gecomp.srv.services.authentication.AuthenticationService;
 
 
 public class AuthenticationBridge {
 
-	private final IAuthenticationService service = new AuthenticationService();
-	
+	private IAuthenticationService service;
+	public IAuthenticationService getService() { return service; }
+	public void setService(IAuthenticationService service) { this.service = service; }
+
 	public final UserView login(String username, String password) throws GeCompException {
-		return new UserView(service.login(username, password));
+		//IAuthenticationService authenticationClient = (IAuthenticationService) GecompContextFactory.getContext().getBean("authenticationClient");
+		return new UserView(getService().login(username, password));
 	}
 	
 }
