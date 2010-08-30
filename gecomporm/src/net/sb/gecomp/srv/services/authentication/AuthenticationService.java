7e67794849bcbf96b711a386ff098160c5ad45f6
71862 <71862@localhost>
2010-08-30 17:03:58 +0200

    [71862] - FS#76 - BE : Implementare struttura a servizi

--- gecomporm/src/net/sb/gecomp/srv/services/authentication/AuthenticationService.java
+++ gecomporm/src/net/sb/gecomp/srv/services/authentication/AuthenticationService.java
@@ -5,6 +5,7 @@ import javax.jws.WebService;
 import net.sb.gecomp.commons.exceptions.GeCompSrvException;
 import net.sb.gecomp.commons.model.User;
 import net.sb.gecomp.commons.services.IAuthenticationService;
+import net.sb.gecomp.commons.utils.Eval;
 import net.sb.gecomp.srv.orm.dao.UserDao;
 
 import org.apache.log4j.Logger;
@@ -23,11 +24,20 @@ public class AuthenticationService implements IAuthenticationService {
 
 		User result = null;
 		long before = System.currentTimeMillis();
+		validate(username, password);
 		result = getDao().isAutheticated(username, password);
 		long after = System.currentTimeMillis();
 		
 		logger.info("Login operation for user "+username+"] took time : "+(after - before)+"ms");
 		return result;
+	}
+	
+	private static final String TO_BE_REMOVED_RECALLED = "XXX.parametri.richiesti.non.specificati.XXX";//TODO:ridefinire!!!
+	private void validate(String username, String password) throws GeCompSrvException {
+		if (Eval.isEmpty(username) || Eval.isEmpty(password)) {
+			logger.error(TO_BE_REMOVED_RECALLED);
+			throw new GeCompSrvException(TO_BE_REMOVED_RECALLED);
+		}
 	}
 	
 }
