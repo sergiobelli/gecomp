79d2a7fcfcc27a2cb5e74782758367a4f1893366
71862 <71862@localhost>
2010-09-03 17:13:19 +0200

    [71862] - FS#76 - BE : Implementare struttura a servizi

--- gecompshared/src/net/sb/gecomp/commons/services/ICompetizioneService.java
+++ gecompshared/src/net/sb/gecomp/commons/services/ICompetizioneService.java
@@ -1,7 +1,22 @@
 package net.sb.gecomp.commons.services;
 
+import java.util.List;
+
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+import net.sb.gecomp.commons.exceptions.GeCompSrvException;
 import net.sb.gecomp.commons.model.Competizione;
 
-public interface ICompetizioneService extends IService<Competizione> {
+@WebService
+public interface ICompetizioneService /*extends IService<Competizione>*/ {
 
+	Competizione save(@WebParam(name="competizione") Competizione competizione) throws GeCompSrvException;
+
+	Competizione get(@WebParam(name="id") Long id) throws GeCompSrvException;
+	
+	void delete(@WebParam(name="id") Long id) throws GeCompSrvException;
+	
+	List<Competizione> list() throws GeCompSrvException; 
+	
 }
