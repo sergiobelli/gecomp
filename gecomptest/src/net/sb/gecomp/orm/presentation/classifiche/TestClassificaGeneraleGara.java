65260958e68481b6b5a4b0063400141a429e2693
sergiobelli81 <sergiobelli81@localhost>
2011-07-06 12:17:13 +0200

    71862

--- gecomptest/src/net/sb/gecomp/orm/presentation/classifiche/TestClassificaGeneraleGara.java
+++ gecomptest/src/net/sb/gecomp/orm/presentation/classifiche/TestClassificaGeneraleGara.java
@@ -17,16 +17,16 @@ import net.sb.gecomp.srv.orm.presentation.classifiche.ClassificaGeneraleGara;
 public class TestClassificaGeneraleGara {
 	public static void main(String[] args) {
 		try {
-			Gara gara = DbManagerFactory.getInstance().getGaraDao().get(1l);
-			List<Prestazione> prestazioni = DbManagerFactory.getInstance().getPrestazioneDao().list(gara);
-			
-			ClassificaGeneraleGara classificaGara = ClassificaCompetizioneManager.getInstance().getClassificaGeneraleGara(gara, prestazioni);
-			System.out.println(classificaGara.getClassificaGeneraleGara());
-			for (Prestazione p : classificaGara.getClassificaGeneraleGara()) {
-				System.out.println(
-						p.getIscrizione().getAtleta().getCognome() + " " + p.getIscrizione().getAtleta().getNome()
-						+ "->" + p.getValoreMisura());
-			}
+//			Gara gara = DbManagerFactory.getInstance().getGaraDao().get(1l);
+//			List<Prestazione> prestazioni = DbManagerFactory.getInstance().getPrestazioneDao().list(gara);
+//			
+//			ClassificaGeneraleGara classificaGara = ClassificaCompetizioneManager.getInstance().getClassificaGeneraleGara(gara, prestazioni);
+//			System.out.println(classificaGara.getClassificaGeneraleGara());
+//			for (Prestazione p : classificaGara.getClassificaGeneraleGara()) {
+//				System.out.println(
+//						p.getIscrizione().getAtleta().getCognome() + " " + p.getIscrizione().getAtleta().getNome()
+//						+ "->" + p.getValoreMisura());
+//			}
 		} catch (GeCompOrmException e) {
 			e.printStackTrace();
 		}
